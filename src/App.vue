cf1c6ac693d6ff65bc6059459d33f1988feb7ccd
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-08-03 23:42:56 +0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: #080808;
+  color:#fff;
+}
+a{
+  color: inherit;
+  text-decoration: none;
+}
+ul{
+  list-style: none;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <img alt="Logo" src=".././assets/logo.png">
+    <ul>
+      <li><a href="#">vue-router</a></li>
+      <li><a href="#">vuex</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.header{
+  width: 100%;
+  height: 100vh;
+}
+ul {
+  display: flex;
+}
+</style>
